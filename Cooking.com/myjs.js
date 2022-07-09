91c5a1e7380d9f9b97c47a0799992981ca1490c1
rifkit <rieyfkiey@gmail.com>
2022-07-09 15:58:44 +0700

    v3

--- Cooking.com/myjs.js
+++ Cooking.com/myjs.js
@@ -3,8 +3,8 @@
   const hidden = document.querySelector('.nav-menu');
 
   menu.addEventListener('click', function (e) {
-      e.preventDefault();
-      hidden.classList.toggle('hidden');
+    e.preventDefault();
+    hidden.classList.toggle('hidden');
   })
 
   // search
@@ -12,38 +12,40 @@
   let search = document.querySelector('#search');
 
   searchIcon.addEventListener('click', function () {
-      searchIcon.style.transform = 'scale(0)';
-      search.style.width = '300px';
-      search.style.height = '40px';
+    searchIcon.style.transform = 'scale(0)';
+    search.style.width = '300px';
+    search.style.height = '40px';
   })
 
   window.addEventListener('scroll', function () {
-      searchIcon.style.transform = '';
-      search.style.width = '';
-      search.style.height = '';
+    searchIcon.style.transform = '';
+    search.style.width = '';
+    search.style.height = '';
   });
 
   // slider
-  let scrollLeft = document.getElementById('btn-left');
-  let scrollRight = document.getElementById('btn-right');
-  let displayScroll = document.getElementById('slider-container');
+  let prev = document.querySelectorAll('.prev');
+  let next = document.querySelectorAll('.next');
+  let displayScroll = document.querySelectorAll('.slider-items');
   let mediaQuery = window.matchMedia('(min-width: 1024px)')
 
   if (mediaQuery.matches) {
-      scrollLeft.classList.remove('hidden');
-      scrollRight.classList.remove('hidden');
+    for (let i = 0; i < displayScroll.length; i++) {
+      prev[i].classList.remove('hidden')
+      next[i].classList.remove('hidden')
 
-      scrollLeft.addEventListener('click', function () {
-          displayScroll.scrollLeft -= window.innerWidth;
-      });
-      scrollRight.addEventListener('click', function () {
-          displayScroll.scrollLeft += window.innerWidth;
-      });
+      prev[i].addEventListener('click', function () {
+        displayScroll[i].scrollLeft -= window.innerWidth;
+      })
+      next[i].addEventListener('click', function () {
+        displayScroll[i].scrollLeft += window.innerWidth;
+      })
+    }
   }
 
   //LazyLoad
   const loading = document.querySelectorAll('img');
 
   for (const lazy of loading) {
-      lazy.setAttribute('loading', 'lazy');
+    lazy.setAttribute('loading', 'lazy');
   }
